318238e9198d0b181c2ae0c84368ac0c92331c38
ms-jade03 <ms_jade_03_0501@yahoo.co.jp>
2024-10-22 21:45:16 +0900

    管理者側の会員管理機能の実装

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\RestaurantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,10 @@ require __DIR__.'/auth.php';
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'auth:admin'], function () {
     Route::get('home', [Admin\HomeController::class, 'index'])->name('home');
+});
 
-    Route::resource('users', UserController::class)->only(['index', 'show']);
+Route::middleware('auth')->prefix('admin')->group(function () {
+    Route::middleware('can:admin')->group(function () {
+        Route::resource('users', UserController::class)->only(['index', 'show']);
+    });
 });
